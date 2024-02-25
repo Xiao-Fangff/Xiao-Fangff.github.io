6274b50cdadfd9a05d304aaf8087a4e7a233c4a2
XiaoFang <1597733125@qq.com>
2024-02-25 22:59:38 +0800

    Site updated: 2024-02-25 22:59:35

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/3eeb.html","posts/bd9a.html"];function toRandomPost(){
+var posts=["posts/hello-world.html","posts/bd9a.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
